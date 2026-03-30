5b645ae2f298c5916e2dfd07dfd3663673771938
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-03-30 14:27:15 +0200

    Update Dockerfile and exercise script; add second.html for testing

--- exercise-2/exercise-2.sh
+++ exercise-2/exercise-2.sh
@@ -12,4 +12,11 @@ exit
 curl http://localhost:2222
 docker stop my-nginx-container
 docker rm my-nginx-container
-docker run 
+docker run -d -p 2222:80 --name my-nginx-container my-nginx:latest
+curl http://localhost:2222
+docker ps
+docker cp my-nginx-container:/usr/share/nginx/html/index.html ./index.html
+cat index.html
+docker tag my-nginx:latest registry.it-scholar.com/bencoeppicus/my-nginx:latest
+docker push registry.it-scholar.com/bencoeppicus/my-nginx:latest
+docker images
