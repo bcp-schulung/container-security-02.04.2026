872d46fe67a46a62ca1e523f120749ceb5ea48a5
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-03-31 10:02:24 +0200

    Add script to run Falco container with necessary volume mounts

---
+++ exercise-6/exercise-6.sh
@@ -0,0 +1,9 @@
+docker run --rm -it \
+           --name falco \
+           --privileged \
+           -v /sys/kernel/tracing:/sys/kernel/tracing:ro \
+           -v /var/run/docker.sock:/host/var/run/docker.sock \
+           -v /proc:/host/proc:ro \
+           -v /etc:/host/etc:ro \
+           falcosecurity/falco:0.43.0
+
