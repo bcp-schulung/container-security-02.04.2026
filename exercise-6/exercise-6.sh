4383959d62ce932bcf0c10ef495f85ebf8a44fe7
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-03-31 10:15:02 +0200

    Add command to create a test file for Falco rule and update Falco container run command

--- exercise-6/exercise-6.sh
+++ exercise-6/exercise-6.sh
@@ -8,3 +8,15 @@ docker run --rm -it \
            falcosecurity/falco:0.43.0
 
 sudo cat /etc/shadow
+
+docker run --rm -it \
+           --name falco \
+           --privileged \
+           -v /sys/kernel/tracing:/sys/kernel/tracing:ro \
+           -v /var/run/docker.sock:/host/var/run/docker.sock \
+           -v /proc:/host/proc:ro \
+           -v /etc:/host/etc:ro \
+           -v $(pwd)/falco_custom_rules.yaml:/etc/falco/falco_rules.local.yaml \
+           falcosecurity/falco:0.43.0
+
+sudo touch /etc/test_file_falco_rule
